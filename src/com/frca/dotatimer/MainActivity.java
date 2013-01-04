ddf3aa1d6da2b037c2b6068becd79cc90899c941
Frca <killerfrca@gmail.com>
2013-01-04 23:24:46 +0100

    Implement changing of timer
    Signed-off-by: Frca <killerfrca@gmail.com>

--- src/com/frca/dotatimer/MainActivity.java
+++ src/com/frca/dotatimer/MainActivity.java
@@ -1,64 +1,72 @@
 package com.frca.dotatimer;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.format.Time;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.frca.dotatimer.helper.Constants;
 import com.frca.dotatimer.helper.HttpRequestHandler;
 import com.frca.dotatimer.helper.ParameterMap;
 import com.frca.dotatimer.helper.Preferences;
 
-import android.os.Bundle;
-import android.os.Handler;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.graphics.Paint;
-import android.text.format.Time;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
 public class MainActivity extends Activity
 {
-    private Timer timer = new Timer();
-    private Handler handler = new Handler();
-    private Runnable runnable = new Runnable()
+    private final Timer timer = new Timer();
+    private final Handler handler = new Handler();
+    private final Runnable runnable = new Runnable()
     {
+        @Override
         public void run()
         {
             refreshLayout();
         }
     };
-    
+
     private String targetText;
     private String targetAuthor;
     private String mainText;
     private String mainAuthor;
     private boolean isDeleted;
-    
+
     public Preferences preferences;
 
-    
+    Calendar timeDatePicker;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         preferences = new Preferences(this);
-        
+
         loadOptions();
-        
+
         refreshStart();
     }
 
@@ -68,7 +76,7 @@ public class MainActivity extends Activity
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
@@ -81,7 +89,7 @@ public class MainActivity extends Activity
                 return super.onOptionsItemSelected(item);
         }
     }
-    
+
     private void onValuesChanged()
     {
         Button deleteButton = (Button)findViewById(R.id.button_delete);
@@ -102,20 +110,21 @@ public class MainActivity extends Activity
     public void planRefresh()
     {
         timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
             public void run()
             {
                 refreshValues();
                 handler.post(runnable);
             }
         }, 0, 1000);
-        
+
     }
-    
+
     public void refreshValues()
     {
         targetText = getTargetTimeString();
         isDeleted = preferences.isDeleted();
-        
+
         if (preferences.isDeleted())
         {
             mainText = preferences.getDeleteReason();
@@ -128,16 +137,16 @@ public class MainActivity extends Activity
             targetAuthor = null;
             mainAuthor = preferences.getSetBy();
         }
-        
+
     }
-    
+
     public void refreshLayout()
     {
         TextView target = (TextView)findViewById(R.id.text_target);
         target.setText(targetText);
         if (isDeleted)
             target.setPaintFlags(target.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
-        
+
         TextView remaining = (TextView)findViewById(R.id.text_main);
         remaining.setText(mainText);
 
@@ -159,32 +168,33 @@ public class MainActivity extends Activity
         else
             main_author.setVisibility(View.GONE);
     }
-    
+
     private void loadOptions()
     {
         if (preferences.getNick() == null)
             createChangeNickDialog();
         else
             Toast.makeText(MainActivity.this, "Vítejte " + preferences.getNick(), Toast.LENGTH_LONG).show();
-        
+
         onValuesChanged();
     }
-    
+
     private void createChangeNickDialog()
     {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
- 
+
         alertDialogBuilder.setTitle("Nastavení nicku");
- 
+
         final EditText input = new EditText(this);
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         input.setLayoutParams(lp);
-        
+
         alertDialogBuilder
             .setMessage("Nastavení tvojí pøezdívky")
             .setCancelable(false)
             .setView(input)
             .setPositiveButton("Potvrdit", new DialogInterface.OnClickListener() {
+                @Override
                 public void onClick(DialogInterface dialog, int id) {
                     String new_nick = input.getText().toString().trim();
                     if (new_nick != "")
@@ -194,10 +204,11 @@ public class MainActivity extends Activity
                     }
                     else
                         Toast.makeText(MainActivity.this, "Neplatný nick", Toast.LENGTH_LONG).show();
-                    
+
                 }
             })
             .setNegativeButton("Zrušit", new DialogInterface.OnClickListener() {
+                @Override
                 public void onClick(DialogInterface dialog,int id) {
                     if (preferences.getNick() == null)
                         finish();
@@ -211,68 +222,70 @@ public class MainActivity extends Activity
         if (preferences.getNick() == null)
             dialog.getButton(Dialog.BUTTON_NEGATIVE).setEnabled(false);
     }
-    
+
     private String getTargetTimeString()
     {
         long target = (long)preferences.getTimer()*1000;
         if (target == 0)
             return "";
-        
+
         Date targetDate = new Date(target);
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault());
         return sdf.format(targetDate);
     }
-    
+
     private String getTargetRemaining()
     {
         long target = (long)preferences.getTimer()*1000;
         if (target == 0)
             return "";
-        
+
         Time timeTarget = new Time();
         timeTarget.set(target);
-        
+
         Time timeNow = new Time();
         timeNow.setToNow();
         long comp = timeTarget.toMillis(true)-timeNow.toMillis(true);
-        return timePassed((int)(comp/1000));        
+        return timePassed((int)(comp/1000));
     }
-    
+
     private static String timePassed(int time)
     {
         boolean positive = time >= 0;
         if (!positive)
             time *= -1;
-        
+
         String value = Constants.toHumanReadable(time);
         String prepand = positive ? "Zbývá" : "Uplynulo";
         return prepand + " " + value;
     }
-    
+
     private void createDeleteDialog()
     {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
- 
+
         alertDialogBuilder.setTitle("Zrušení timeru");
- 
+
         final EditText input = new EditText(this);
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         input.setLayoutParams(lp);
-        
+
         alertDialogBuilder
             .setMessage("Zadej dùvod pro zrušení souèasného timeru")
             .setCancelable(false)
             .setView(input)
             .setPositiveButton("Potvrdit", new DialogInterface.OnClickListener() {
+                @Override
                 public void onClick(DialogInterface dialog, int id) {
                     String deleteReason = input.getText().toString().trim();
                     ParameterMap params = new ParameterMap();
                     params.put(Constants.TAG_DELETE_REASON, deleteReason);
                     params.put(Constants.TAG_DELETE_BY, preferences.getNick());
-                    new HttpRequestHandler(MainActivity.this).execute(params);                    
+                    new HttpRequestHandler(MainActivity.this).execute(params);
                 }
             })
             .setNegativeButton("Zrušit", new DialogInterface.OnClickListener() {
+                @Override
                 public void onClick(DialogInterface dialog,int id) {
                     dialog.cancel();
                 }
@@ -283,28 +296,52 @@ public class MainActivity extends Activity
         if (preferences.getNick() == null)
             dialog.getButton(Dialog.BUTTON_NEGATIVE).setEnabled(false);
     }
-    
+
     public void callRefresh(View v)
     {
         refreshStart();
     }
-    
+
     public void callChange(View v)
     {
-        
+        timeDatePicker = Calendar.getInstance();
+        new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,int dayOfMonth) {
+                timeDatePicker.set(Calendar.YEAR, year);
+                timeDatePicker.set(Calendar.MONTH, monthOfYear);
+                timeDatePicker.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hour, int minute)
+                    {
+                        timeDatePicker.set(Calendar.HOUR_OF_DAY, hour);
+                        timeDatePicker.set(Calendar.MINUTE, minute);
+                        timeDatePicker.set(Calendar.SECOND, 0);
+
+                        int time = (int) (timeDatePicker.getTimeInMillis()/1000);
+                        ParameterMap params = new ParameterMap();
+                        params.put(Constants.TAG_TIMER, Integer.toString(time));
+                        params.put(Constants.TAG_SET_BY, preferences.getNick());
+                        new HttpRequestHandler(MainActivity.this).execute(params);
+                    }
+
+                }, timeDatePicker.get(Calendar.HOUR), timeDatePicker.get(Calendar.MINUTE), true).show();
+            }
+        }, timeDatePicker.get(Calendar.YEAR), timeDatePicker.get(Calendar.MONTH), timeDatePicker.get(Calendar.DAY_OF_MONTH)).show();
     }
-    
+
     public void callDelete(View v)
     {
         createDeleteDialog();
     }
-    
+
     public void refreshStart()
     {
         ((Button)findViewById(R.id.button_refresh)).setEnabled(false);
         new Synchronization(this).execute();
     }
-    
+
     public void refreshComplete()
     {
         ((Button)findViewById(R.id.button_refresh)).setEnabled(true);
