86a0509cc365518cc4323456ddab6f61d9494c57
Frca <killerfrca@gmail.com>
2013-01-16 01:38:13 +0100

    Php: Properly set ErrorPresenter, fully implement read and create Api part
    Signed-off-by: Frca <killerfrca@gmail.com>

---
+++ php/app/ApiModule/classes/ApiResponser.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace ApiModule;
+
+use Nette\Application\Responses\TextResponse,
+    Nette\Application\Responses\JsonResponse;
+
+class ApiResponser {
+
+    public static function returnBadRequest($additional="") {
+        self::getResponse(DataPresenter::$instance)->setCode(\Nette\Http\Response::S400_BAD_REQUEST);
+        $response = "Bad request";
+        if ($additional)
+            $response .= " - '".$additional."'";
+
+        DataPresenter::$instance->sendResponse(new TextResponse($response));
+    }
+
+    public static function returnUnauthorized($additional="") {
+        self::getResponse(DataPresenter::$instance)->setCode(\Nette\Http\Response::S401_UNAUTHORIZED);
+        $response = "Unauthorized";
+        if ($additional)
+            $response .= " - '".$additional."'";
+
+        DataPresenter::$instance->sendResponse(new TextResponse($response));
+    }
+
+    public static function returnForbidden($additional="") {
+        self::getResponse(DataPresenter::$instance)->setCode(\Nette\Http\Response::S403_FORBIDDEN);
+        $response = "Forbidden";
+        if ($additional)
+            $response .= " - '".$additional."'";
+
+        DataPresenter::$instance->sendResponse(new TextResponse($response));
+    }
+
+    public static function returnOK() {
+        self::getResponse(DataPresenter::$instance)->setCode(\Nette\Http\Response::S200_OK);
+        DataPresenter::$instance->sendResponse(new JsonResponse(self::passwordlessJSON(DataPresenter::$instance->json)));
+    }
+
+    public static function returnCreated() {
+        self::getResponse(DataPresenter::$instance)->setCode(201);
+        DataPresenter::$instance->sendResponse(new JsonResponse(self::passwordlessJSON(DataPresenter::$instance->json)));
+    }
+
+    public static function returnAccepted() {
+        self::getResponse(DataPresenter::$instance)->setCode(202);
+        DataPresenter::$instance->sendResponse(new JsonResponse(self::passwordlessJSON(DataPresenter::$instance->json)));
+    }
+
+    private static function getResponse() {
+        return DataPresenter::$instance->getService('httpResponse');
+    }
+
+    private static function passwordlessJSON($json) {
+        DataPresenter::setVar($json, Tags::$TAG_CHANNEL_PASS);
+        return $json;
+    }
+}
+
+
--- php/app/ApiModule/presenters/DataPresenter.php
+++ php/app/ApiModule/presenters/DataPresenter.php
@@ -1,187 +1,186 @@
 <?php
 namespace ApiModule;
 
-use \Tags as Tags;
+use \Nette\Application\ForbiddenRequestException,
+    \Nette\Application\BadRequestException,
+    \stdClass as JSONObject;
 
 class DataPresenter extends \BasePresenter {
-
-
-    private $json;
+    public $json;
 
     private $id;
     private $nick;
     private $password;
 
-    public function actionRead($format, $data, $id, $query = array(), $associations = array()) {
-        $this->setInitial($id, $query);
-        $this->createBaseJson();
-        try {
-            $this->checkPassword($id, $query);
-        } catch (\Nette\Application\ForbiddenRequestException $e) {
-            echo "Forbidden - ".$e->getMessage() ;
-        } catch (\Nette\Application\BadRequestException $e) {
-            echo "BadRequest - ".$e->getMessage();
-        }
+    public static $instance;
+
+    public function startup() {
+        self::$instance = $this;
+        parent::startup();
     }
 
-    public function renderRead() {
-
+    public function actionRead($format, $data, $id, $query = array(), $associations = array()) {
+        try {
+            $this->setInitial($id, $query);
+            $this->loadLocalJSON();
+            $this->checkPassword();
+            ApiResponser::returnOK();
+        } catch (ForbiddenRequestException $e) {
+            ApiResponser::returnForbidden($e->getMessage());
+        } catch (BadRequestException $e) {
+            ApiResponser::returnBadRequest($e->getMessage());
+        }
     }
 
     public function actionCreate($format, $data, $id, $query = array(), $associations = array()) {
-        $this->setInitial($id, $data);
-        $localData = $this->getLocalJSON($this->id);
-        if ($localData == NULL)
-            $this->json = $this->createBaseJson($this->id, $this->password);
-        else {
-            try {
+        try {
+            $this->setInitial($id, $data);
+
+            $this->loadLocalJSON();
+            if ($this->json == NULL)
+                $this->json = $this->createBaseJson();
+            else
                 $this->checkPassword();
-            } catch (\Nette\Application\ForbiddenRequestException $e) {
-                $this->redirect(400, "Error:400");
-            }
+
+            $this->addDataToJson($this->json, self::createUserData($this->nick, "", ""));
+            $this->saveJson();
+
+            ApiResponser::returnCreated();
+        } catch (ForbiddenRequestException $e) {
+            ApiResponser::returnForbidden($e->getMessage());
+        } catch (BadRequestException $e) {
+            ApiResponser::returnBadRequest($e->getMessage());
         }
-
-        $this->addDataToJson($this->json, self::createUserData($this->nick, "", ""));
-        $this->saveJson();
-        $this->redirect(201, "Error:201");
-    }
-
-    public function renderCreate() {
-
     }
 
     public function actionUpdate($format, $data, $id, $query = array(), $associations = array()) {
         $this->setInitial($id, $data);
     }
 
-    public function renderUpdate() {
-        $this->saveJson();
-        $this->redirect(202, "Error:202");
-    }
-
 
     private function setInitial($id, $request) {
-        $this->id = $id;
-        try {
-            $this->password = $this->setFromRequest($request);
-        } catch (\Nette\Application\ForbiddenRequestException $e) {
-            $this->redirect(400, "Error:", 400);
-        }
-    }
-    private function checkPassword() {
-        $receivedPass = $this->password;
-        $storedPass = $this->getStoredPassword();
-        if ($storedPass != $receivedPass)
-            throw new \Nette\Application\ForbiddenRequestException("Password does not match saved password.");
-
-        return $receivedPass;
-    }
-
-    private function setFromRequest($request) {
         if (is_array($request)) {
+            $this->id = $id;
+
             if (!array_key_exists(Tags::$TAG_CHANNEL_PASS, $request))
-                throw new \Nette\Application\ForbiddenRequestException("Password is required.");
+                throw new ForbiddenRequestException("Array: Password is required.");
             $this->password = $request[Tags::$TAG_CHANNEL_PASS];
 
             if (!array_key_exists(Tags::$TAG_NICK, $request))
-                throw new \Nette\Application\ForbiddenRequestException("Author nick is required.");
+                throw new BadRequestException("Array: Author nick is required.");
             $this->nick = $request[Tags::$TAG_NICK];
 
-        } elseif(is_object($request)) {
-            $data = json_decode($request);
+        } elseif(is_string($request) && is_object($data = json_decode($request))) {
+            $fieldId = Tags::$TAG_CHANNEL_NAME;
+            if (!isset($data->$fieldId))
+                throw new ForbiddenRequestException("JSON: Id is required.");
+            $this->id = $data->$fieldId;
+
             $fieldPass = Tags::$TAG_CHANNEL_PASS;
             if (!isset($data->$fieldPass))
-                throw new \Nette\Application\ForbiddenRequestException("Password is required.");
-            $this->password = $request->$fieldPass;
+                throw new ForbiddenRequestException("JSON: Password is required.");
+            $this->password = $data->$fieldPass;
 
             $fieldNick = Tags::$TAG_NICK;
             if (!isset($data->$fieldNick))
-                throw new \Nette\Application\ForbiddenRequestException("Author nick is required.");
-            $this->nick = $request->$fieldNick;
+                throw new BadRequestException("JSON: Author nick is required.");
+            $this->nick = $data->$fieldNick;
         }
+        else
+            throw new BadRequestException("Bad values supplied.");
     }
 
-    private function getLocalJSON($id) {
+    private function loadLocalJSON() {
         if ($this->json == NULL) {
-            $filename = DATA_DIR . $this->idToFileName($id);
+            $filename =  $this->getRouteToJSON($this->id);
             if (file_exists($filename)) {
                 $fileContent = file_get_contents($filename);
                 $this->json = json_decode($fileContent);
             }
+            else
+                $this->json = NULL;
         }
 
         return $this->json;
     }
 
+    private function checkPassword() {
+        $receivedPass = $this->password;
+        $storedPass = $this->getStoredPassword();
+        if ($storedPass != $receivedPass)
+            throw new ForbiddenRequestException("Password does not match saved password.");
+
+        return $receivedPass;
+    }
+
     private function getStoredPassword() {
-        $json = $this->getLocalJSON($this->id);
-        if (json == NULL)
-            throw new \Nette\Application\BadRequestException("Channel with id '".  $this->id."' does not exist.");
+        $this->loadLocalJSON();
+        if ($this->json == NULL)
+            throw new BadRequestException("Channel with id '".  $this->id."' does not exist.");
 
         $field = Tags::$TAG_CHANNEL_PASS;
-        return $json->$field;
+        return $this->json->$field;
     }
 
-    private function idToFileName($id) {
-        return $id . ".json";
+    private function getRouteToJSON($id) {
+        return DATA_DIR . '/' . $id . ".json";
     }
 
-    private function createBaseJson($channelName, $channelPass) {
-        $valueNickPair = new \stdClass();
-        $this->setVar($valueNickPair, Tags::$TAG_NICK);
-        $this->setVar($valueNickPair, Tags::$TAG_VALUE);
+    private function createBaseJson() {
+        $valueNickPair = new JSONObject();
+        self::setVar($valueNickPair, Tags::$TAG_NICK);
+        self::setVar($valueNickPair, Tags::$TAG_VALUE);
 
-        $newJson = new \stdClass();
-        $this->setVar($newJson, Tags::$TAG_CHANNEL_NAME, $channelName);
-        $this->setVar($newJson, Tags::$TAG_CHANNEL_PASS, $channelPass);
-        $this->setVar($newJson, Tags::$TAG_TIMER, $valueNickPair);
-        $this->setVar($newJson, Tags::$TAG_DELETE, $valueNickPair);
-        $this->setVar($newJson, Tags::$TAG_USERS, array());
+        $newJson = new JSONObject();
+        self::setVar($newJson, Tags::$TAG_CHANNEL_NAME, $this->id);
+        self::setVar($newJson, Tags::$TAG_CHANNEL_PASS, $this->password);
+        self::setVar($newJson, Tags::$TAG_CHANGED, time());
+        self::setVar($newJson, Tags::$TAG_TIMER, $valueNickPair);
+        self::setVar($newJson, Tags::$TAG_DELETE, $valueNickPair);
+        self::setVar($newJson, Tags::$TAG_USERS, array());
 
         return $newJson;
     }
 
-    private function setVar($stdClass, $variableName, $value = "") {
+    public static function setVar($stdClass, $variableName, $value = "") {
         $stdClass->$variableName = $value;
-        $changed = Tags::$TAG_CHANGED;
-        $stdClass->$changed = time();
     }
 
-    private function getVar($stdClass, $variableName) {
+    private static function getVar($stdClass, $variableName) {
         return $stdClass->$variableName;
     }
 
-    private function addDataToJson($json, $userJson)
-    {
+    private function addDataToJson($json, $userJson) {
         $usersTag = Tags::$TAG_USERS;
         $allUsers = &$json->$usersTag;
-        $newNick = $this->getVar($userJson, Tags::$TAG_NICK);
+        $newNick = self::getVar($userJson, Tags::$TAG_NICK);
         $key = $this->getUserKeyInArray($allUsers, $newNick);
 
         if ($key === -1)    // not found
-            $key = "";
-
-        $allUsers[$key] = $userJson;
+            $allUsers[] = $userJson;
+        else
+            $allUsers[$key] = $userJson;
     }
 
     private function getUserKeyInArray($users, $newNick) {
         foreach ($users as $key => $user)
-            if ($this->getVar($user, Tags::$TAG_NICK) == $newNick)
+            if (self::getVar($user, Tags::$TAG_NICK) == $newNick)
                 return $key;
 
         return -1;
     }
 
     private static function createUserData($nick, $state, $reason) {
-        $data = new \stdClass();
-        $this->setVar($data, Tags::$TAG_NICK, $nick);
-        $this->setVar($data, Tags::$TAG_STATE, $state);
-        $this->setVar($data, Tags::$TAG_REASON, $reason);
+        $data = new JSONObject();
+        self::setVar($data, Tags::$TAG_NICK, $nick);
+        self::setVar($data, Tags::$TAG_STATE, $state);
+        self::setVar($data, Tags::$TAG_REASON, $reason);
         return $data;
     }
 
     private function saveJson() {
-        $file = fopen($this->idToFileName($this->id), "w");
+        self::setVar($this->json, Tags::$TAG_CHANGED, time());
+        $file = fopen($this->getRouteToJSON($this->id), "w");
         fwrite($file, json_encode($this->json));
         fclose($file);
     }
