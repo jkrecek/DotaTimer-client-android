79e01e4ef7504684d273b59a061924d12e20a469
Frca <killerfrca@gmail.com>
2013-01-21 00:07:14 +0100

    PHP: Change Tags variable types (thx Audio)

--- php/app/ApiModule/classes/Tags.php
+++ php/app/ApiModule/classes/Tags.php
@@ -3,18 +3,18 @@ namespace ApiModule;
 
 final class Tags {
 
-    public static $TAG_NICK = "nick";
-    public static $TAG_VALUE = "value";
+    const $TAG_NICK = "nick";
+    const $TAG_VALUE = "value";
 
-    public static $TAG_CHANNEL_NAME = "channel_name";
-    public static $TAG_CHANNEL_PASS = "channel_pass";
-    public static $TAG_CHANGED = "changed";
+    const $TAG_CHANNEL_NAME = "channel_name";
+    const $TAG_CHANNEL_PASS = "channel_pass";
+    const $TAG_CHANGED = "changed";
 
-    public static $TAG_TIMER = "timer";
-    public static $TAG_DELETE = "delete";
+    const $TAG_TIMER = "timer";
+    const $TAG_DELETE = "delete";
 
-    public static $TAG_USERS = "users";
+    const $TAG_USERS = "users";
 
-    public static $TAG_STATE = "state";
-    public static $TAG_REASON = "reason";
+    const $TAG_STATE = "state";
+    const $TAG_REASON = "reason";
 }
