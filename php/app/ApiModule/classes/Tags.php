9f2c9ede464bcae91ead3eb5dfb40ba28aebc254
Frca <killerfrca@gmail.com>
2013-01-21 00:29:40 +0100

    PHP: Update typo in Tags

--- php/app/ApiModule/classes/Tags.php
+++ php/app/ApiModule/classes/Tags.php
@@ -3,18 +3,18 @@ namespace ApiModule;
 
 final class Tags {
 
-    const $TAG_NICK = "nick";
-    const $TAG_VALUE = "value";
+    const TAG_NICK = "nick";
+    const TAG_VALUE = "value";
 
-    const $TAG_CHANNEL_NAME = "channel_name";
-    const $TAG_CHANNEL_PASS = "channel_pass";
-    const $TAG_CHANGED = "changed";
+    const TAG_CHANNEL_NAME = "channel_name";
+    const TAG_CHANNEL_PASS = "channel_pass";
+    const TAG_CHANGED = "changed";
 
-    const $TAG_TIMER = "timer";
-    const $TAG_DELETE = "delete";
+    const TAG_TIMER = "timer";
+    const TAG_DELETE = "delete";
 
-    const $TAG_USERS = "users";
+    const TAG_USERS = "users";
 
-    const $TAG_STATE = "state";
-    const $TAG_REASON = "reason";
+    const TAG_STATE = "state";
+    const TAG_REASON = "reason";
 }
